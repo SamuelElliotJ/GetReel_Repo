2f4c5fa2cebfbb67da52bf5a86334168521631be
Samuel Johnson <SamuelElliotJ@Github.io>
2024-03-26 15:49:31 +0000

    Missing UProject - Fix

---
+++ Source/GetReel_V0/GetReel_V0.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GetReel_V0.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GetReel_V0, "GetReel_V0" );
+
+DEFINE_LOG_CATEGORY(LogGetReel_V0)
+ 
---
+++ Source/GetReel_V0/GetReel_V0GameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GetReel_V0GameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGetReel_V0GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGetReel_V0GameMode();
+};
+
+
+
