eae29659a9d58898b3adebf7c25baf82a5ba60e2
Samuel Johnson <SamuelElliotJ@Github.io>
2024-04-08 02:54:16 +0100

    GetReel V1 Demo Update
    Whats Changed:
    - Cleaned up folders to make navigation clearer
    - Added placeholder UI for demonstration purposes
    - Added fishing minigame
    - Merged day and night clock system with placeholder UI
    - Moved proto character blueprints to playable characters with hierachy system
    - Created a game instance to spawn selected character
    - Created a custom player start
    
    TO DO:
    - Merge Eve-Simpson fish catching mechanic with new minigame system
    - Merge Anisur-Rahman inventory [Fish Database] mechanic with current placeholder UI
    - Clean up blueprints / make minigame mechanic more modular
    - Package game / make a playable build
    
    08/04/2024

---
+++ Source/GetReel_V0/GI_GetReel.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "GI_GetReel.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GETREEL_V0_API UGI_GetReel : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
