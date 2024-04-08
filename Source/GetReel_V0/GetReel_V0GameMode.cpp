b5016168cfebf11ff702d8928b34be48c8fd472c
Samuel Johnson <SamuelElliotJ@Github.io>
2024-04-08 07:24:40 +0100

    GetReel V1 Demo Update #2
    Whats Changed:
    - Merged Eve-Simpson fish catching mechanic with minigame system
    - Merged Eve-Simpson fish catching mechanic with spear system
    - Packaged the game
    
    TO DO:
    - Clean up blueprints / make minigame mechanic more modular
    
    08/04/2024

--- Source/GetReel_V0/GetReel_V0GameMode.cpp
+++ Source/GetReel_V0/GetReel_V0GameMode.cpp
@@ -11,11 +11,11 @@ AGetReel_V0GameMode::AGetReel_V0GameMode()
 	PlayerControllerClass = AGetReel_V0PlayerController::StaticClass();
 
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDown/Blueprints/BP_Human_Character"));
-	if (PlayerPawnBPClass.Class != nullptr)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
+	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT(""));
+	//if (PlayerPawnBPClass.Class != nullptr)
+	//{
+	//	DefaultPawnClass = PlayerPawnBPClass.Class;
+	//}
 
 	// set default controller to our Blueprinted controller
 	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownPlayerController"));
