424391bad1c057c975c109d818ebe307327e4be4
Samuel Johnson <SamuelElliotJ@Github.io>
2024-04-25 23:00:31 +0100

    Save System Implemented
    - Added a save system with 3 save slots
    - Game Saves at the end of each day
    
    - Added shop menu - Needs to be updated with items
    - Added placeholder pause menu

---
+++ Source/GetReel_V0/DataAsset_ShopItem.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#pragma once
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "DataAsset_ShopItem.generated.h"
+
+// Data Asset Class For Shop Items
+
+UCLASS()
+class GETREEL_V0_API UDataAsset_ShopItem : public UPrimaryDataAsset
+{
+public:	
+	GENERATED_BODY()
+	// Item ID
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	uint8 ItemID;
+
+	// Item Name
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FName ItemName;
+
+	// Item Image
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UTexture2D* ItemImage;
+
+	// Item Mesh ()
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UStaticMesh* ItemMesh;
+
+	// Item Description
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FString ItemDescription;
+
+	// Item Cost
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float ItemCost;
+};
